f6d26803521dd4323dcf3cfa3522e4919c13d214
takehito sato <takehitosato@gmail.com>
2015-10-15 20:54:36 +0900

    bundle gem slack-courier

---
+++ lib/slack/courier.rb
@@ -0,0 +1,7 @@
+require "slack/courier/version"
+
+module Slack
+  module Courier
+    # Your code goes here...
+  end
+end
---
+++ lib/slack/courier/version.rb
@@ -0,0 +1,5 @@
+module Slack
+  module Courier
+    VERSION = "0.1.0"
+  end
+end
